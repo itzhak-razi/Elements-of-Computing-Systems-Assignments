c7069966dea978c021eea21851eb3da50d37366c
U-PC-4815162342\Jonathan Nanney <Jonathan Nanney@pc-4815162342.(none)>
2010-02-22 20:03:50 -0500

    The multiplication assembly program works.  It's the simplest algorithm not the most efficient.

--- project04/mult/Mult.asm
+++ project04/mult/Mult.asm
@@ -1,4 +1,27 @@
-/*
- *This is a Hack assembly language program to multiply the numbers in registers
- * R0 and R1
- */
+ //This is a Hack assembly language program to multiply the numbers in registers
+ //R0 and R1
+
+@R2
+M=0
+
+(LOOP)
+    @R1
+    D = M
+    @INFINITE_LOOP
+    D;JLE
+
+
+    @R0
+    D = M
+    @R2
+    M = D + M
+
+    @R1
+    M = M - 1
+    D = M
+    @LOOP
+    0;JMP
+
+(INFINITE_LOOP)
+    @INFINITE_LOOP
+    0;JMP 
