1b606b4fcb9f54f8ec349f1c729629deaf3a8821
U-PC-4815162342\Jonathan Nanney <Jonathan Nanney@pc-4815162342.(none)>
2010-02-23 21:44:43 -0500

    I have a more functional version of the fill program now.  Still not perfect.  It only fills the first row of the screen and for some reason it never gets the first little section blackened.

--- project04/fill/Fill.asm
+++ project04/fill/Fill.asm
@@ -1,28 +1,48 @@
-//This program should be an infinite loop that listens for keyboard input.  
-//When the user presses any key the screen is darkened.
+@16416
+D = A
+@R1 //Maximum screen value
+M = D // Sets R1 to 16416, the laste byte that the screen uses
+
 
 @SCREEN
 D = A
 @R0
 M = D
 
-(INFINITE_LOOP)
-@0
-D = !A
-@R0
-A = M
-M = D
-
-@R0
-M = M + 1
-
-(KEYBOARD_TEST) 
-@KBD
-D=M
 @KEYBOARD_TEST
-D;JEQ //If keyboard == 0 then loop again.
-
-
-
-@INFINITE_LOOP
 0;JMP
+
+(RESTART)
+    @0
+    D = !A //Set D to all 1 bits
+    @R0 //Technically not necessary since R0 and @0 are the same but I like to be explicit
+    A = M
+    M = D
+
+(KEYBOARD_TEST)
+    @R0
+    M = M + 1 // Increment the screen pointer
+    //Check to see if M > maximum screen byte and if so reset to @screen 
+    @R1
+    D = M
+    @R0
+    D = D - M
+    @RESET
+    D;JLE
+
+    @KBD
+    D=M
+    @RESTART
+    D;JNE
+    
+    @KEYBOARD_TEST
+    0;JMP
+
+(RESET)
+    @SCREEN
+    D = A
+    @R0
+    M = D 
+
+    @KEYBOARD_TEST 
+    0;JMP
