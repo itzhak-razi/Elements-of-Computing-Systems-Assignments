c839900c98841c945dcac6264a3b14282c9e226e
Jonathan Nanney <jnanney1207@gmail.com>
2010-03-22 17:39:00 -0400

    Implemented the dest function in the code module and started on the comp function but I'm trying to think of a more elegant solution.

--- project06/code.py
+++ project06/code.py
@@ -1,2 +1,20 @@
-def dest(value)
-    
+def dest(value):
+    #Do not change order in array
+    possible = ["A", "D", "M"]
+    result = ""
+        for current in possible:
+            if current in value:
+                result += "1"
+            else:
+                result += "0" 
+    return result
+
+
+def comp(value):
+    result = ""
+    if "M" in value:
+        result += "1"
+    elif "A" in value:
+        result += "0"
+
+    return result
