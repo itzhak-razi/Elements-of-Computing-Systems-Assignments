c96e718eaabccf0838ff0d2016db87697190378e
Jonathan Nanney <jnanney1207@gmail.com>
2010-05-01 13:15:15 -0400

    Wrote the function to determine the jump binary

--- project06/code.py
+++ project06/code.py
@@ -14,7 +14,17 @@ def comp(value):
     result = ""
     if "M" in value:
         result += "1"
-    elif "A" in value:
+    else:
         result += "0"
 
+    return result
+
+def jump(value):
+    import re
+    possible = ["L|NE|MP", "[^N]E|MP", "G|NE|MP"]
+    for current in possible:
+        if re.search(current, value):
+            result += "1"
+        else:
+            result += "0"
     return result
