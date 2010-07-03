1e39e0a90119599d8d0c4037865fcf23e6872e75
Jonathan Nanney <jnanney1207@gmail.com>
2010-07-03 14:57:54 -0400

    git status tells me that codeWriter has had some changes so I'm committing it.  I have no idea what these changes are since I haven't worked on this in a while.  I really should have made the commit whenever I did the work.

--- project07/codeWriter.py
+++ project07/codeWriter.py
@@ -2,19 +2,53 @@ class CodeWriter:
 
     def __init__(self, fileOut):
         self.outputFile = fileOut
+        self.outputFile.write("@255")
+        self.outputFile.write("D=A")
+        self.outputFile.write("@SP")
+        self.outputFile.write("M=D")
 
     def setFileName(self, fileName):
         self.currentName = fileName
 
     def writeArithmetic(self, command):
-        
+        if command == "add":
+            pop(self)
+            self.outputFile.write("@SP")
+            self.outputFile.write("M=M+D")
+        elif command == "sub":
+            pop(self)
+            self.outputFile.write("@SP")
+            self.outputFile.write("M=D-M")
+        elif command == "neg":
+            self.outputFile.write("@SP")
+            self.outputFile.write("M=-M")
+        elif command == "eq":
+            pop(self)
+            self.outputFile.write("@SP")
+            self.outputFile
+            
+
     def writePushPop(self, command, segment, index):
         import stackParser
         from stackParser import Parser 
         if command == Parser.C_PUSH:
             if segment == "constant":
                 self.outputFile.write("@" + str(index))
-                self.outputFile.write("M=" + D)
+                self.outputFile.write("D=A")
+                push(self)
+    
+    #Pushes the value in D into the stack
+    def push(self):
+        self.outputFile.write("@SP")
+        self.outputFile.write("M=M+1")
+        self.outputFile.write("A=M")
+        self.outputFile.write("M=D")
+
+    #Pops the stack and puts the value into the D register
+    def pop(self):
+        self.outputFile.write("@SP")
+        self.outputFile.write("D=M")
+        self.outputFile.write("M=M-1")
 
     def close(self):
         self.outputFile.close()
