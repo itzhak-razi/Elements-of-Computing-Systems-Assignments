2b181027c5391fb5a04fe162f4ed41feab557153
Jonathan Nanney <jnanney1207@gmail.com>
2010-08-03 22:02:24 -0400

    Worked on the tokenizer a little.  Trying to think of a simple way to remove /** */ style comments

--- project10/JackTokenizer.py
+++ project10/JackTokenizer.py
@@ -1,3 +1,27 @@
 class JackTokenizer:
-    def __init__(self):
 
+    KEYWORDS = ["class", "constructor", "function", "method", "field", "static", "var", "int",
+                "char", "boolean", "void", "true", "false", "null", "this", "let", "do", "if",
+                "else", "while", "return"]
+
+    SYMBOLS = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<',
+                '>', '=', '~']    
+    
+
+
+    def __init__(self, inputFile):
+       import re
+       fileContents = inputFile.read()
+       re.split("\s+", fileContents)
+
+
+
+    def hasMoreTokens(self):
+        pass
+    def advance(self):
+    def tokenType(self):
+    def keyWord(self):
+    def symbol(self):
+    def identifier(self):
+    def intVal(self):
+    def stringVal(self):
