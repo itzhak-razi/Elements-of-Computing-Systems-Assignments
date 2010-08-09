ca935ec715ce34479a65eff57f44d77a852898f8
Jonathan Nanney <jnanney1207@gmail.com>
2010-08-08 21:42:41 -0400

    I finished all the methods in my Tokenizer class.  Still needs to be tested.

--- project10/JackTokenizer.py
+++ project10/JackTokenizer.py
@@ -6,7 +6,12 @@ class JackTokenizer:
 
     SYMBOLS = ['{', '}', '(', ')', '[', ']', '.', ',', ';', '+', '-', '*', '/', '&', '|', '<',
                 '>', '=', '~']    
-
+    
+    KEYWORD = 0
+    SYMBOL = 1
+    IDENTIFIER = 2
+    INT_CONST = 3
+    STRING_CONST = 4
     
 
 
@@ -14,11 +19,13 @@ class JackTokenizer:
         import re
         fileContents = inputFile.read()
         currentIndex = 0
-        commentsRemoved = ""
         quoteMode = False
+        self.commentsRemoved = ""
         self.currentToken = ""
+        self.tokenIndex = 0
 
-        #Loop to strip comments
+        #Loop to strip comments.  Note that if a file has a beginning comment indicator (// or /*) 
+        #as the last characters this won't remove them.
         while currentIndex < len(fileContents) - 1:
             if fileContents[currentIndex] == "\"":
                 quoteMode = not quoteMode
@@ -31,48 +38,72 @@ class JackTokenizer:
             else:
                 commentsRemoved += fileContents[currentIndex]
                 currentIndex += 1
-        commentsRemoved += fileContents[currentIndex]
+        self.commentsRemoved += fileContents[currentIndex]
         
        
 
     def hasMoreTokens(self):
-        if len(self.spaceSplit) > 0:
+        import re
+        if re.search("[^ ]", self.commentsRemoved[self.tokenIndex + 1 : ])
             return True
-        return False 
+        return False
 
     def advance(self):
-        self.currentToken = self.spaceSplit.pop(0)
-        for currentSymbol in SYMBOLS:
-            if(re.match(currentSymbol, self.currentToken) and 
-               len(currentSymbol) < len(self.currentToken)):
-                self.spaceSplit.insert(0, self.currentToken[len(currentSymbol):])
-                self.currentToken = currentSymbol
-
-        for currentKeyword in KEYWORDS:
-            if(re.match(currentKeyword, self.currentToken) and
-               len(currentKeyword) < len(self.currentToken)):
-                self.spaceSplit.insert(0, self.currentToken[len(currentKeyword):])
-                self.currentToken = currentKeyword 
-
-
+        import re
+        #strip spaces
+        while self.tokenIndex < len(self.commentsRemoved) and 
+              self.commentsRemoved[self.tokenIndex].isspace():
+            self.tokenIndex = self.tokenIndex + 1
+        #match keywords
+        if self.commentsRemoved[self.tokenIndex].isalpha():
+            for currentKeyword in KEYWORDS:
+                if re.match(currentKeyword, self.commentsRemoved[self.tokenIndex:]):
+                    self.currentToken = currentKeyword
+                    self.tokenIndex += len(currentKeyword)
+                    self.tokenType = JackTokenizer.KEYWORD
+                    break
+        #match string constants 
+        elif self.commentsRemoved[self.tokenIndex] == "\"":
+            quoteGroup = re.match("\"[^\"]*\"", self.commentsRemoved[self.tokenIndex:]
+            self.currentToken = quoteGroup.group(0)
+            self.tokenIndex += len(self.currentToken)
+            self.tokenType = JackTokenizer.STRING_CONST
+        #match symbols
+        elif not self.commentsRemoved[self.tokenIndex].isnumeric():
+            for currentSymbol in SYMBOLS:
+                if re.match(currentSymbol, self.commentsRemoved(self.tokenIndex]):
+                    self.tokenIndex += 1
+                    self.tokenType = JackTokenizer.SYMBOL
+        #match integer constants
+        elif self.commentsRemoved[self.tokenIndex].isnumeric():
+            numGroup = re.match("\d+", self.commentsRemoved[self.tokenIndex:])
+            self.currentToken = numGroup.group(0)
+            self.tokenIndex += len(self.currentToken)
+            self.tokenType = JackTokenizer.INT_CONST
+        #match identifier
+        else:
+            identifierMatch = re.match("[a-zA-Z_]+[a-ZA-Z0-9_]*", self.commentsRemoved[self.tokenIndex:]
+            self.currentToken = identifierMatch.group(0)
+            self.tokenIndex += len(self.currentToken)
+            self.tokenType = JackTokenizer.IDENTIFIER
 
     def tokenType(self):
-
+        return self.tokenType
 
     def keyWord(self):
-        pass
+        return self.currentToken
 
     def symbol(self):
-        pass
+        return self.currentToken
 
     def identifier(self):
-        pass
+        return self.currentToken
 
     def intVal(self):
-        pass
+        return self.currentToken
 
     def stringVal(self):
-        pass
+        return self.currentToken[1:len(self.currentToken) - 2] #strips the quotes 
 
     def current(self):
         return self.currentToken
