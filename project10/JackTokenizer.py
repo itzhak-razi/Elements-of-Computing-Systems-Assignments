49872b795b07d53e23163546555550e0fd651712
Jonathan Nanney <jnanney1207@gmail.com>
2010-08-09 22:01:54 -0400

    Fixed a number of errors in my Tokenizer.  There is still one bug when trying to get the last symbol in my test file.

--- project10/JackTokenizer.py
+++ project10/JackTokenizer.py
@@ -29,14 +29,14 @@ class JackTokenizer:
         while currentIndex < len(fileContents) - 1:
             if fileContents[currentIndex] == "\"":
                 quoteMode = not quoteMode
-                commentsRemoved += fileContents[currentIndex]
+                self.commentsRemoved += fileContents[currentIndex]
                 currentIndex += 1
             elif (not quoteMode) and fileContents[currentIndex] == "/" and fileContents[currentIndex + 1] == "/": 
                 currentIndex = fileContents.find("\n", currentIndex + 1)
             elif (not quoteMode) and fileContents[currentIndex] == "/" and fileContents[currentIndex + 1] == "*":
                 currentIndex = fileContents.find("*/", currentIndex + 1) + 2
             else:
-                commentsRemoved += fileContents[currentIndex]
+                self.commentsRemoved += fileContents[currentIndex]
                 currentIndex += 1
         self.commentsRemoved += fileContents[currentIndex]
         
@@ -44,51 +44,66 @@ class JackTokenizer:
 
     def hasMoreTokens(self):
         import re
-        if re.search("[^ ]", self.commentsRemoved[self.tokenIndex + 1 : ])
+        if re.search("[^ ]", self.commentsRemoved[self.tokenIndex + 1 : ]):
             return True
         return False
 
     def advance(self):
         import re
+        matched = False
         #strip spaces
-        while self.tokenIndex < len(self.commentsRemoved) and 
-              self.commentsRemoved[self.tokenIndex].isspace():
-            self.tokenIndex = self.tokenIndex + 1
+        while(self.tokenIndex < len(self.commentsRemoved) and 
+              self.commentsRemoved[self.tokenIndex].isspace()):
+            self.tokenIndex += 1
+
+        if not self.tokenIndex < len(self.commentsRemoved):
+            return
+
         #match keywords
         if self.commentsRemoved[self.tokenIndex].isalpha():
-            for currentKeyword in KEYWORDS:
+            print("Attempting alpha match")
+            for currentKeyword in JackTokenizer.KEYWORDS:
                 if re.match(currentKeyword, self.commentsRemoved[self.tokenIndex:]):
                     self.currentToken = currentKeyword
                     self.tokenIndex += len(currentKeyword)
-                    self.tokenType = JackTokenizer.KEYWORD
+                    self.type = JackTokenizer.KEYWORD
+                    matched = True
                     break
         #match string constants 
-        elif self.commentsRemoved[self.tokenIndex] == "\"":
-            quoteGroup = re.match("\"[^\"]*\"", self.commentsRemoved[self.tokenIndex:]
+        if self.commentsRemoved[self.tokenIndex] == "\"" and not matched:
+            print("Attempting str constants match")
+            quoteGroup = re.match("\"[^\"]*\"", self.commentsRemoved[self.tokenIndex:])
             self.currentToken = quoteGroup.group(0)
             self.tokenIndex += len(self.currentToken)
-            self.tokenType = JackTokenizer.STRING_CONST
+            self.type = JackTokenizer.STRING_CONST
+            matched = True
         #match symbols
-        elif not self.commentsRemoved[self.tokenIndex].isnumeric():
-            for currentSymbol in SYMBOLS:
-                if re.match(currentSymbol, self.commentsRemoved(self.tokenIndex]):
+        if (not self.commentsRemoved[self.tokenIndex].isdigit()) and (not matched):
+            print("Attempting symbols match")
+            for currentSymbol in JackTokenizer.SYMBOLS:
+                if currentSymbol == self.commentsRemoved[self.tokenIndex]:
                     self.tokenIndex += 1
-                    self.tokenType = JackTokenizer.SYMBOL
+                    self.type = JackTokenizer.SYMBOL
+                    self.currentToken = currentSymbol
+                    matched = True
         #match integer constants
-        elif self.commentsRemoved[self.tokenIndex].isnumeric():
+        if self.commentsRemoved[self.tokenIndex].isdigit() and not matched:
+            print("Attempting int constants match")
             numGroup = re.match("\d+", self.commentsRemoved[self.tokenIndex:])
             self.currentToken = numGroup.group(0)
             self.tokenIndex += len(self.currentToken)
-            self.tokenType = JackTokenizer.INT_CONST
+            self.type = JackTokenizer.INT_CONST
+            matched = True
         #match identifier
-        else:
-            identifierMatch = re.match("[a-zA-Z_]+[a-ZA-Z0-9_]*", self.commentsRemoved[self.tokenIndex:]
+        if not matched:
+            print("Attempting identifier match")
+            identifierMatch = re.match("[a-zA-Z_]+[a-zA-Z0-9_]*", self.commentsRemoved[self.tokenIndex:])
             self.currentToken = identifierMatch.group(0)
             self.tokenIndex += len(self.currentToken)
-            self.tokenType = JackTokenizer.IDENTIFIER
+            self.type = JackTokenizer.IDENTIFIER
 
     def tokenType(self):
-        return self.tokenType
+        return self.type
 
     def keyWord(self):
         return self.currentToken
