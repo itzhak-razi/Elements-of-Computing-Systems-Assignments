e63dc9ee22a99a79c9d030a5583c822c2e4a185e
Jonathan Nanney <jnanney1207@gmail.com>
2010-08-14 15:36:12 -0400

    Implemented more methods for my CompilationEngine.  Still haven't tested it.

--- project10/CompilationEngine.py
+++ project10/CompilationEngine.py
@@ -11,7 +11,7 @@ class CompilationEngine:
 
         if tokenizer.currentToken != "class":
             raise Exception("Keyword 'class' expected")
-        outputFile.write("<class>\n")
+        self.outputFile.write("<class>\n")
         i = 0
         while tokenizer.hasMoreTokens() and i < NUM_OPENING_STATEMENTS: 
             self.printToken()
@@ -23,8 +23,10 @@ class CompilationEngine:
         while self.tokenizer.hasMoreTokens() and self.tokenizer.keyWord in subOpenings:
             self.compileSubroutine()
         self.printToken()
+        outputFile.write("</class>\n")
     
     def compileClassVarDec(self):
+        self.outputFile.write("<classVarDec>\n")
         NUM_OPENING_STATEMENTS = 3
         i = 0
         while self.tokenizer.hasMoreTokens() and i < NUM_OPENING_STATEMENTS:
@@ -45,8 +47,10 @@ class CompilationEngine:
 
         if self.tokenizer.hasMoreTokens():
             self.tokenizer.advance()
+        self.outputFile.write("</classVarDec>\n")
 
     def compileSubroutine(self):
+        self.outputFile.write("<subroutineDec>\n")
         NUM_OPENING_STATEMENTS = 4
         i = 0
         while self.tokenizer.hasMoreTokens() and i < NUM_OPENING_STATEMENTS:
@@ -62,16 +66,65 @@ class CompilationEngine:
         self.compileStatements()
         self.printToken()
         self.tokenizer.advance()
-
-
+        self.outputFile.write("</subroutineDec>\n")
 
     def compileParameterList(self):
+        import JackTokenizer
+        from JackTokenizer import JackTokenizer
+        self.outputFile.write("<parameterList>\n")
+        while self.tokenizer.tokenType == JackTokenizer.SYMBOL and self.tokenizer.symbol() != ")":
+            self.printToken()
+            if self.tokenizer.hasMoreTokens():
+                self.tokenizer.advance()
+            self.printToken()
+            if self.tokenizer.hasMoreTokens():
+                self.tokenizer.advance()
+            self.printToken()
+            if self.tokenizer.hasMoreTokens():
+                self.tokenizer.advance()
+        self.outputFile.write("</parameterList>\n")
 
     def compileVarDec(self):
+        self.outputFile.write("<varDec>\n")
+        while self.tokenizer.hasMoreTokens() and 
+                (self.tokenizer.tokenType != JackTokenizer.SYMBOL or self.tokenizer.symbol() != ";"):
+            self.printToken()
+            self.tokenizer.advance()
+        self.printToken()
+        self.tokenizer.advance()
+        self.outputFile.write("</varDec>\n")
 
     def compileStatements(self):
+        self.outputFile.write("<statements>\n")
+        stmtStarts = ['do', 'while', 'let', 'if', 'return']
+        while(self.tokenizer.hasMoreTokens() and self.tokenizer.tokenType == JackTokenizer.KEYWORD 
+              and self.tokenizer.keyWord() in stmtStarts):
+            if self.tokenizer.keyWord() == "do":
+                self.compileDo()
+            elif self.tokenizer.keyWord() == "while":
+                self.compileWhile()
+            elif self.tokenizer.keyWord() == "let":
+                self.compileLet()
+            elif self.tokenizer.keyWord() == "if":
+                self.compileIf()
+            elif self.tokenizer.keyWord() == "return":
+                self.compileReturn()
+        self.outputFile.write("</statements>\n")
 
     def compileDo(self):
+        self.outputFile.write("</doStatement>\n")
+        if self.tokenizer.keyWord() != "do":
+            raise Exception("'do' keyword expected")
+        self.printToken()
+        while (self.tokenizer.hasMoreTokens() and 
+              (self.tokenizer.tokenType != JackTokenizer.SYMBOL or JackTokenizer.tokenType.symbol() != "(")):
+            self.tokenizer.advance() 
+            self.printToken()
+        self.compileExpressionList()
+        self.printToken() #print ')'
+        self.tokenizer.advance()
+        self.printToken() #Print ';'
+        self.outputFile.write("</doStatement>\n")
 
     def compileLet(self):
 
