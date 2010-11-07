c4b5e47ea617e64d306ab8fc177135136904bc72
Jonathan Nanney <jnanney1207@gmail.com>
2010-11-06 21:32:32 -0400

    Fixed a small bug.  Was putting a variable into the wrong hash table.

--- project11/SymbolTable.py
+++ project11/SymbolTable.py
@@ -1,8 +1,9 @@
 class SymbolTable:
-    STATIC = 0
-    FIELD = 1
-    ARG = 2
-    VAR = 3
+    NONE = 0
+    STATIC = 1
+    FIELD = 2
+    ARG = 3
+    VAR = 4
 
     def __init__(self):
         self.classTable = dict()
@@ -12,21 +13,22 @@ class SymbolTable:
         self.subTable = dict()
    
     def define(self, name, identifierType, identifierKind):
+        print("Name is " + str(name) + " type is " + str(identifierType) + " and kind is " + str(identifierKind))
         varIndex = self.varCount(identifierKind)
-        if(kind == SymbolTable.STATIC || kind == SymbolTable.FIELD):
-            self.classTable[name] = dict(kind=identifierKind, type=identifierKind, index=varIndex)
+        if(identifierKind == SymbolTable.STATIC or identifierKind == SymbolTable.FIELD):
+            self.classTable[name] = dict(kind=identifierKind, type=identifierType, index=varIndex)
         else:
-            self.subTable[name] = dict(kind=identifierKind, type=identifierKind, index=varIndex)
+            self.subTable[name] = dict(kind=identifierKind, type=identifierType, index=varIndex)
             
     def varCount(self, kind):
         total = 0
-        if(kind == SymbolTable.STATIC || kind == SymbolTable.FIELD):
+        if(kind == SymbolTable.STATIC or kind == SymbolTable.FIELD):
             for key in self.classTable.keys():
                 if self.classTable[key]["kind"] == kind:
                     total += 1
         else:
             for key in self.subTable.keys():
-                if self.classTable[key]["kind"] == kind:
+                if self.subTable[key]["kind"] == kind:
                     total += 1
         return total
 
@@ -48,3 +50,15 @@ class SymbolTable:
         else:
             raise Exception(name + " not found in either hash table")
 
+    def stringKindOf(self, name):
+        constKind = self.kindOf(name)
+        if constKind == SymbolTable.STATIC:
+            return "static"
+        elif constKind == SymbolTable.FIELD:
+            return "field"
+        elif constKind == SymbolTable.ARG:
+            return "arg"
+        elif constKind == SymbolTable.VAR:
+            return "var"
+        else:
+            return "none"
